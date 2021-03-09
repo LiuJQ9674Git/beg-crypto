0b65c03b30e91538df3a0dca8c4445488e8d3db6
LiuJQ9674Git <455298138@qq.com>
2021-03-09 23:20:41 +0800

    调整结构

--- src/test/java/com/abc/crypto/tools/demo/chapter7/certificates/OAEPExample.java
+++ src/test/java/com/abc/crypto/tools/demo/chapter7/certificates/OAEPExample.java
@@ -2,14 +2,17 @@ package com.abc.crypto.tools.demo.chapter7.certificates;
 
 import java.security.GeneralSecurityException;
 import java.security.KeyPair;
+import java.security.Security;
 
 import javax.crypto.SecretKey;
 
+import com.abc.signature.RsaUtils;
+import org.bouncycastle.jce.provider.BouncyCastleProvider;
 import org.bouncycastle.util.Arrays;
 
 import static com.abc.signature.RsaUtils.generateRSAKeyPair;
-import static com.abc.crypto.tools.demo.chapter7.certificates.RsaUtils.keyUnwrapOAEP;
-import static com.abc.crypto.tools.demo.chapter7.certificates.RsaUtils.keyWrapOAEP;
+//import static com.abc.crypto.tools.demo.chapter7.certificates.RsaUtils.keyUnwrapOAEP;
+//import static com.abc.crypto.tools.demo.chapter7.certificates.RsaUtils.keyWrapOAEP;
 import static com.abc.crypto.tools.demo.chapter7.certificates.Utils.createTestAESKey;
 
 /**
@@ -17,6 +20,9 @@ import static com.abc.crypto.tools.demo.chapter7.certificates.Utils.createTestAE
  */
 public class OAEPExample
 {
+    static {
+        Security.addProvider(new BouncyCastleProvider());
+    }
     public static void main(String[] args)
         throws GeneralSecurityException
     {
